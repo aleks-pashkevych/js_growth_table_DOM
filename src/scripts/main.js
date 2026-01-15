333eeee6256ee25c0f6312212ed01ac23243dda4
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-01-15 09:56:02 +0200

    task working, not all test passed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,69 @@
 'use strict';
 
 // write code here
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+const table = document.querySelector('tbody');
+let rowsNumber = table.childElementCount;
+let columnsNumber = table.firstChild.childElementCount;
+
+const removeTable = () => {
+  while (table.firstChild) {
+    table.firstChild.remove();
+  }
+};
+
+const drawColumns = () => {
+  for (let i = 0; i < rowsNumber; i++) {
+    let tr = '<tr>';
+    let cells = '';
+
+    for (let j = 0; j < columnsNumber; j++) {
+      cells += `<td></td>`;
+    }
+    tr += cells + '</tr>';
+    table.innerHTML += tr;
+  }
+};
+
+appendRow.addEventListener('click', (e) => {
+  if (rowsNumber < 10) {
+    rowsNumber++;
+    removeTable();
+    drawColumns();
+  } else {
+    e.target.disabled = true;
+  }
+});
+
+removeRow.addEventListener('click', (e) => {
+  if (rowsNumber > 2) {
+    rowsNumber--;
+    removeTable();
+    drawColumns();
+  } else {
+    e.target.disabled = true;
+  }
+});
+
+appendColumn.addEventListener('click', (e) => {
+  if (columnsNumber < 10) {
+    columnsNumber++;
+    removeTable();
+    drawColumns();
+  } else {
+    e.target.disabled = true;
+  }
+});
+
+removeColumn.addEventListener('click', (e) => {
+  if (columnsNumber > 2) {
+    columnsNumber--;
+    removeTable();
+    drawColumns();
+  } else {
+    e.target.disabled = true;
+  }
+});
